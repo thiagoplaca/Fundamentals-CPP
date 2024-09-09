dd059069489bf6d7f989d2f017c0404082db5e01
Thiago Placa <thiago_placa99@outlook.com>
2024-09-09 20:25:10 -0300

    Initial C++

---
+++ C++Basics/first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a,b,c;
+  cout<<"Enter 2 Numbers.";
+  cin>>a>>b;
+  c = a + b;
+  cout<<"Addition is "<< c;
+
+  return 0;
+
+}
---
+++ C++Basics/name.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string name;
+
+  cout<<"May I Know your name: ";
+  cin>>name;
+  cout<<"Welcome Mr."<< name;
+
+  return 0;
+}
